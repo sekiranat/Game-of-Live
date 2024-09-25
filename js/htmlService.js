9f6c3c7046b5a866c975c7da5ec0db3fdcd72ba5
Sergey Nikulin <s.nikulin@fairtech.ru>
2024-09-25 14:44:29 +0500

    Генерация поля

--- js/htmlService.js
+++ js/htmlService.js
@@ -1,19 +1,43 @@
 class HtmlService {
   constructor() {
+    this.widthCell = 10 + 2; // размер cell + border width в css
     this.userInterface = `
-          <div class="liveGame__interface">
-              <label class="liveGame__interface-label" for="field-size">
-                  <div class="liveGame__interface-text">
-                      Количество клеток
-                  </div>
-                  <input class="liveGame__interface-input" name="field-size" type="text">
-              </label>
-          </div>
+    <div class="liveGame__interface">
+        <h1 class="liveGame__interface-title">
+            Игра жизнь
+        </h1>
+        <div class="liveGame__interface-form">
+            <label class="liveGame__interface-label" for="field-size">
+                <div class="liveGame__interface-text">
+                    Количество клеток
+                </div>
+                <input data-type="field-size" class="liveGame__interface-input" name="field-size" type="text">
+            </label>
+            <button data-action="start" class="liveGame__interface-button">Применить</button>
+        </div>
+    </div>
     `;
   }
 
   getHtmlByKeyName(name) {
-    return this[name]
+    return this[name];
+  }
+
+  getMapHtml(size) {
+    const countCells = size * size;
+    const mapContainer = document.createElement("div");
+    mapContainer.classList.add("map");
+
+    for(var i = 0; i < countCells; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        mapContainer.append(cell);
+    }
+
+    mapContainer.style.width = `${this.widthCell * size}px`;
+    mapContainer.style.height = `${this.widthCell * size}px`;
+
+    return mapContainer;
   }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import HtmlService from './htmlService.js'
+import HtmlService from "./htmlService.js";
 
 class Live {
   constructor(name) {
@@ -6,10 +6,37 @@ class Live {
     this.subscribers = [];
     this.gameRoot = document.getElementById("game-root");
     this.htmlService = new HtmlService();
+    this.mapSize = [100, 100];
   }
 
   init() {
     this.buildInterface();
+    this.setListeners();
+  }
+
+  setListeners() {
+    const startButton = document.querySelector('[data-action="start"]');
+    startButton.addEventListener("click", (e) => {
+        const target = e.target;
+        if (target === startButton) {
+            const fieldSize = document.querySelector('[data-type="field-size"]').value
+            this.setMapSize(fieldSize)
+            this.generateMap(fieldSize);
+        }
+    });
+  }
+
+  setMapSize(size) {
+    this.mapSize = [size, size];
+  }
+
+  generateMap(size) {
+    const htmlMap = this.htmlService.getMapHtml(size);
+    const mapElement = document.getElementById('map')
+
+    if (mapElement) this.gameRoot.removeChild(mapElement)
+    console.log(htmlMap)
+    this.gameRoot.appendChild(htmlMap)
   }
 
   buildInterface() {
