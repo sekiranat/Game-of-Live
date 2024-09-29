0d9deef7a54b11a48e9e798d042ae4de75264bed
Sergey Nikulin <s.nikulin@fairtech.ru>
2024-09-29 14:03:28 +0500

    Правка стилей формы, вывод подсказки про задержку рендеринга

--- js/htmlService.js
+++ js/htmlService.js
@@ -1,24 +1,6 @@
 class HtmlService {
   constructor() {
     this.widthCell = 10 + 2; // размер cell + border width в css
-    this.gameInterface = `
-    <div class="liveGame__interface">
-        <h1 class="liveGame__interface-title">
-            Игра жизнь
-        </h1>
-        <div class="liveGame__interface-form">
-            <label class="liveGame__interface-label" for="field-size">
-                <div class="liveGame__interface-text">
-                    Количество клеток
-                </div>
-                <input data-type="board-size" class="liveGame__interface-input" name="field-size" type="text">
-            </label>
-            <button data-action="apply" class="liveGame__interface-button">Применить</button>
-            <button data-action="start" class="liveGame__interface-button">Запустить</button>
-        </div>
-    </div>
-    `;
-
     this.canvas = `
     <canvas id="game-board" class="liveGame__board"/>
     `;
@@ -39,14 +21,23 @@ class HtmlService {
             Игра жизнь
         </h1>
         <div class="liveGame__interface-form">
-            <label class="liveGame__interface-label" for="field-size">
+         <div class="liveGame__interface-form-top">    
+        <label class="liveGame__interface-label" for="field-size">
                 <div class="liveGame__interface-text">
                     Количество клеток
                 </div>
                 <input data-type="board-size" class="liveGame__interface-input" value=${defaultBoardSize} name="field-size" type="text">
             </label>
+            <div class="liveGame__interface-form-buttons">
             <button data-action="apply" class="liveGame__interface-button">Применить</button>
             <button data-action="start" class="liveGame__interface-button">Запустить</button>
+            </div>
+</div>
+<div class="liveGame__interface-form-bottom">
+                       <div class="liveGame__interface-form-bottom">
+              Если количество клеток меньше 800, рендерится с таймаутом 400мс
+           </div> 
+              </div> 
         </div>
     </div>
     `;
